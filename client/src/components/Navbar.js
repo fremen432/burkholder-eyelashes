8995f62fbd5a41c0f38d2a8ffc8238492f701b2c
somdobomk <somdobomk@gmail.com>
2022-01-14 03:27:11 -0600

    added hero section

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,7 +13,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Nav() {
+export default function Navbar() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
