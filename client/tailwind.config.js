1047d192eb141aea8ef5a4d64c0fcdfb9eb300bb
somdobomk <somdobomk@gmail.com>
2022-01-15 17:07:04 -0600

    updated config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{html,js}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    }
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
