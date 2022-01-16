d103bad21fdfd8d45f8a5f7b681f6418ba3630e4
nguyen-william93 <=>
2022-01-16 00:29:15 -0600

    export connection.js and set up server.js to recieve and send information

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/burkholder-eyelashes', 
     {
         useNewUrlParser: true, 
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        userCreateIndex: true,
+        useFindAndModify: true
     }
 );
+
+module.exports = mongoose.connection;
