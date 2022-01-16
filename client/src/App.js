aa511ac1314225b8d01542e7f3413e0b78e8c257
somdobomk <somdobomk@gmail.com>
2022-01-16 12:22:28 -0600

    updated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Products from "./components/Products";
-import Footer from "./components/Footer";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <Products />
-      <Footer />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
