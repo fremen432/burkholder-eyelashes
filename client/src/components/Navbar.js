f106b940c12de797377eb9ab300c81a561355698
somdobomk <somdobomk@gmail.com>
2022-01-15 21:45:58 -0600

    updated navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -19,12 +19,12 @@ export default function Navbar() {
                   <img
                     className="block lg:hidden h-8 w-auto"
                     src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt="Workflow"
+                    alt="Company Logo"
                   />
                   <img
                     className="hidden lg:block h-8 w-auto"
                     src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                    alt="Workflow"
+                    alt="Company Logo"
                   />
                 </div>
                 <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
