a8c109325360304124366d170761467f04ba6d16
Mickey Phillips <phillipsmickey41@gmail.com>
2022-01-17 16:23:55 -0600

    adding user and user login work server side

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth')
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -10,7 +11,8 @@ const app = express();
 const startServer = async () => {
   const server = new ApolloServer({ 
     typeDefs, 
-    resolvers
+    resolvers,
+    context: authMiddleware
   });
 
   await server.start();
