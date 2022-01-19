d9b64a8d1f79b77deb8fd771badab022cd7902f9
nguyen-william93 <=>
2022-01-19 17:05:44 -0600

    auth.js for front end

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -26,8 +26,8 @@ module.exports = {
 
     return req;
   },
-  signToken: function ({ firstName, email, _id }) {
-    const payload = { firstName, email, _id };
+  signToken: function ({ username, email, role_id }) {
+    const payload = { username, email, role_id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
