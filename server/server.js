374b525a88d6f1ddf78ea053f318a56987a728c3
nguyen-william93 <=>
2022-01-20 18:45:02 -0600

    add path

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { authMiddleware } = require('./utils/auth')
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
+const path = require ('path')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
